0408b55f616540f744cd89f60177d9502d945faf
Kelly L <phyulwin73929@gmail.com>
2024-07-11 00:56:21 -0700

    Background Added

--- Src/Application.py
+++ Src/Application.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import customtkinter
-from PIL import ImageTk, Image
+import PIL
+from PIL import Image, ImageTk
 
 from Utility import print_default_error_message, copy_to_clipboard, exit_program
 from gemini import askGemini
@@ -15,34 +16,48 @@ class Application:
         customtkinter.set_appearance_mode("light")
         customtkinter.set_default_color_theme("dark-blue")
 
-        self.root.geometry("960x540")
+        self.root.geometry("1262x710")
         self.root.title("Python CustomTkinter Interface for Gemini API with Alexa Integration")
 
-        frame = customtkinter.CTkFrame(master=self.root)
-        frame.pack(pady=20, padx=60, fill="both", expand=True)
+        # Configure app background
+        image = PIL.Image.open("Assets/application_background.png")
+        background_image = customtkinter.CTkImage(image, size=(1262, 710))
 
-        self.prompt_input_frame = customtkinter.CTkEntry(master=frame, placeholder_text="enter prompt here",
+        def bg_resizer(e):
+            if e.widget is self.root:
+                i = customtkinter.CTkImage(image, size=(e.width, e.height))
+                bg_lbl.configure(text="", image=i)
+
+        # Create a background label
+        bg_lbl = customtkinter.CTkLabel(self.root, text="", image=background_image)
+        bg_lbl.place(x=0, y=0)
+
+        self.root.bind("<Configure>", bg_resizer)
+        # Background added!
+
+        self.prompt_input_frame = customtkinter.CTkEntry(master=self.root, placeholder_text="enter prompt here",
                                                         width=200, height=150)
         self.prompt_input_frame.pack(pady=12, padx=10)
 
-        self.prompt_output_frame = customtkinter.CTkLabel(master=frame, text="display out here", font=("Roboto", 11))
+        self.prompt_output_frame = customtkinter.CTkLabel(master=self.root, text="display output here", font=("Roboto", 11),
+                                                          fg_color="transparent")
         self.prompt_output_frame.pack(pady=12, padx=10)
 
         self.current_response = ""
 
-        enter_prompt_btn = customtkinter.CTkButton(master=frame, text="enter prompt", command=self.request_response)
+        enter_prompt_btn = customtkinter.CTkButton(master=self.root, text="enter prompt", command=self.request_response)
         enter_prompt_btn.pack(pady=12, padx=10)
 
-        copy_output_btn = customtkinter.CTkButton(master=frame, text="copy output", command=self.copy_response)
+        copy_output_btn = customtkinter.CTkButton(master=self.root, text="copy output", command=self.copy_response)
         copy_output_btn.pack(pady=12, padx=10)
 
-        voice_input_btn = customtkinter.CTkButton(master=frame, text="voice input", command=self.voice_input)
+        voice_input_btn = customtkinter.CTkButton(master=self.root, text="voice input", command=self.voice_input)
         voice_input_btn.pack(pady=12, padx=10)
 
-        exit_btn = customtkinter.CTkButton(master=frame, text="exit program", command=exit_program)
+        exit_btn = customtkinter.CTkButton(master=self.root, text="exit program", command=exit_program)
         exit_btn.pack(pady=12, padx=10)
 
-        clear_text_btn = customtkinter.CTkButton(master=frame, text="new prompt", command=self.clear_text)
+        clear_text_btn = customtkinter.CTkButton(master=self.root, text="new prompt", command=self.clear_text)
         clear_text_btn.pack(pady=12, padx=10)
 
     def request_response(self):
@@ -81,10 +96,10 @@ class Application:
     def return_response(self, new_text):
         self.prompt_output_frame.configure(text=new_text)
 
-if __name__ == "__main__":
-    root = customtkinter.CTk()
-    app = Application(root)
-    try:
-        root.mainloop()
-    except Exception as e:
-        print_default_error_message(e)
+# if __name__ == "__main__":
+#     root = customtkinter.CTk()
+#     app = Application(root)
+#     try:
+#         root.mainloop()
+#     except Exception as e:
+#         print_default_error_message(e)
--- Src/main.py
+++ Src/main.py
@@ -2,7 +2,6 @@ import customtkinter
 from Application import Application
 from Utility import print_default_error_message
 
-
 def main():
     try:
         root = customtkinter.CTk()
